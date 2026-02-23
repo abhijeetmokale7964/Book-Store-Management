d09a6dea75b068eb47b62948ba50558cdfeae99c
abhijeetmokale7964 <abhijeetmokale7964@gmail.com>
2026-02-23 10:51:00 +0530

    Update Book.java

--- src/main/java/com/bookStore/entity/Book.java
+++ src/main/java/com/bookStore/entity/Book.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.bookStore.entity;
 
 import jakarta.persistence.Entity;
@@ -67,78 +67,3 @@ public class Book {
 
 	
 }
-=======
-package com.bookStore.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
-public class Book {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-
-	private String name;
-	
-	private String author;
-	
-	private String price;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public String getPrice() {
-		return price;
-	}
-
-	public void setPrice(String price) {
-		this.price = price;
-	}
-
-	public Book(int id, String name, String author, String price) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.author = author;
-		this.price = price;
-	}
-
-	public Book() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", name=" + name + ", author=" + author + ", price=" + price + "]";
-	}
-	
-	
-	
-}
->>>>>>> 14cc1d7e61fc807763dc644a7324efaae564d440
