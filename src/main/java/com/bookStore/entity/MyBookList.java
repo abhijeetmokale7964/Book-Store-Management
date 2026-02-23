2a2f53a0a4d0dfcefb0fc1476d26e68d30d4e451
abhijeetmokale7964 <abhijeetmokale7964@gmail.com>
2026-02-23 10:50:10 +0530

    Update MyBookList.java

--- src/main/java/com/bookStore/entity/MyBookList.java
+++ src/main/java/com/bookStore/entity/MyBookList.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.bookStore.entity;
 
 import jakarta.persistence.*;
@@ -97,42 +97,3 @@ public class MyBookList {
     
     
 }
-=======
-package com.bookStore.entity;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "my_books")
-public class MyBookList {
-
-    @Id
-    private int id;
-
-    private String name;
-    private String author;
-    private String price;
-
-    // getters & setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
-
-    public String getPrice() { return price; }
-    public void setPrice(String price) { this.price = price; }
-
-    public MyBookList() {}
-
-    public MyBookList(int id, String name, String author, String price) {
-        this.id = id;
-        this.name = name;
-        this.author = author;
-        this.price = price;
-    }
-}
->>>>>>> 14cc1d7e61fc807763dc644a7324efaae564d440
